49ce5c7361b31977a63578dca9f2d5d3477a5f60
NoGitNoLife <111093017+NoGitNoLife@users.noreply.github.com>
2024-11-29 18:03:01 -0500

    actualizado completo

--- homework/clean_data.py
+++ homework/clean_data.py
@@ -61,50 +61,41 @@ def create_normalized_key(df):
     return df
 
 
-def generate_cleaned_text(df):
-    """Crea la columna 'cleaned' en el DataFrame"""
 
-    df = df.copy()
-    #df_orig = df.copy()
+def generate_cleaned_text(df):
+    """Crea la columna 'cleaned_text' en el DataFrame"""
+
+    keys = df.copy()
 
     # Ordene el dataframe por 'key' y 'text'
-    df = df.sort_values(by=["key", "text"], ascending=[True, True])    
+    keys = keys.sort_values(by=["key", "raw_text"], ascending=[True, True])
 
     # Seleccione la primera fila de cada grupo de 'key'
     keys = df.drop_duplicates(subset="key", keep="first")
 
     # Cree un diccionario con 'key' como clave y 'text' como valor
-    # [(adhoc queri, AD-HOC QUERIES), (agricultur product, AGRICULTURAL PRODUCTION), ... ]
-    key_dict = dict(zip(keys["key"], keys["text"]))    
+    key_dict = dict(zip(keys["key"], keys["raw_text"]))
 
     # Cree la columna 'cleaned' usando el diccionario
-    #df_orig["cleaned"] = df_orig["key"].map(key_dict)
-    df["cleaned"] = df["key"].map(key_dict)
+    df["cleaned_text"] = df["key"].map(key_dict)
 
     return df
-
 
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
 
     df = df.copy()
-    df = df[["cleaned"]]
-    df = df.rename(columns={"cleaned": "text"})
+    df = df[["raw_text", "cleaned_text"]]
     df.to_csv(output_file, index=False)
 
-
-#
-# Orquestador:
-#
 def main(input_file, output_file):
     """Ejecuta la limpieza de datos"""
 
     df = load_data(input_file)
-    df = create_key(df)
+    df = create_normalized_key(df)
     df = generate_cleaned_text(df)
     df.to_csv("files/test.csv", index=False)
-    save_data(df, output_file)
-
+    save_data(df, output_file)    
 
 
 
@@ -112,4 +103,4 @@ if __name__ == "__main__":
     main(
         input_file="files/input.txt",
         output_file="files/output.txt",
-    )    
+    )   
